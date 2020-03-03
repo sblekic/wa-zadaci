6ef9f59f6e0ef082f1dbea4e755c18409e632919
Nikola Tanković <nikola.tankovic@gmail.com>
2020-03-03 11:03:48 +0100

    Week 2 final

--- src/index.js
+++ src/index.js
@@ -8,7 +8,26 @@ const port = 3000  // port na kojem će web server slušati
 app.use(cors())
 
 app.get('/posts', (req, res) => {
-    res.json(storage.posts)
-});
+    let posts = storage.posts
+    let query = req.query
+    
+    if (query.title) {
+        posts = posts.filter(e => e.title.indexOf(query.title) >= 0)
+    }
+    
+    if (query.createdBy) {
+        posts = posts.filter(e => e.createdBy.indexOf(query.createdBy) >= 0)
+    }
+    
+    if (query._any) {
+        let terms = query._any.split(" ")
+        posts = posts.filter(doc => {
+            let info = doc.title + " " + doc.createdBy
+            return terms.every(term => info.indexOf(term) >= 0)
+        })
+    }
+
+    res.json(posts)
+})
 
 app.listen(port, () => console.log(`Slušam na portu ${port}!`))
